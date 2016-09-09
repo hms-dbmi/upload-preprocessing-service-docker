2217f51eb576efe727690877b6fe7c6df268679b
mtmcduffie <mcduffie.michael@gmail.com>
2016-09-09 15:11:51 -0400

    UDNTECH-624 - Need to flush output to get logging to docker.

--- poll_process.py
+++ poll_process.py
@@ -16,10 +16,10 @@ sqs = boto3.resource('sqs')
 queue = sqs.get_queue_by_name(QueueName=currentQueue)
 
 while True:
-	print("Retrieving messages from queue - '" + currentQueue + "'")
+	print("Retrieving messages from queue - '" + currentQueue + "'", flush=True)
 	
 	for message in queue.receive_messages(MessageAttributeNames=['UDN_ID','FileBucket','FileKey','DestinationBucket','DestinationKey']):
-		print("Found Messages, processing.")
+		print("Found Messages, processing.", flush=True)
 
 		if message.message_attributes is not None:
 			UDN_ID = message.message_attributes.get('UDN_ID').get('StringValue')
@@ -31,7 +31,7 @@ while True:
 			DestinationKey = message.message_attributes.get('DestinationKey').get('StringValue')
 			
 			if UDN_ID and FileBucket and FileKey and DestinationBucket and DestinationKey:
-				print("Processing UDN_ID - " + UDN_ID + ".")
+				print("Processing UDN_ID - " + UDN_ID + ".", flush=True)
 				
 				#Get the new ID to use instead of the UDN_ID.
 				request_params = {"udn_id" : UDN_ID}
@@ -40,10 +40,10 @@ while True:
 					r = requests.get('http://ups.aws.dbmi.hms.harvard.edu/id_pair/create_or_retrieve/',params=request_params)
 					external_id = r.json()[0]["external_id"]
 				except:
-					print("Error retrieving external ID - ", sys.exc_info()[0])
+					print("Error retrieving external ID - ", sys.exc_info()[0], flush=True)
 
 				if(external_id):
-					print("Retrieved external ID.")
+					print("Retrieved external ID.", flush=True)
 
 					#Generate file IDs and paths.
 					tempBAMFile = str(uuid.uuid4())
@@ -53,19 +53,19 @@ while True:
 					
 					process_bam = True
 					
-					print("Downloading file from S3.")
+					print("Downloading file from S3.", flush=True)
 					
 					#Retrieve the file from S3.
 					try:
 						retrieveBucket = resource.Bucket(FileBucket)
 						retrieveBucket.download_file(FileKey, tempBAMFile)
 					except:
-						print("Error retrieving file from S3 - ", sys.exc_info()[0])				
+						print("Error retrieving file from S3 - ", sys.exc_info()[0], flush=True)				
 						process_bam = False		
 					
 					if(process_bam):
 					
-						print("Processing BAM with samtools.")
+						print("Processing BAM with samtools.", flush=True)
 						
 						try:
 							#Now we need to swap the ID's via samtools. Do some crazy piping.
@@ -78,9 +78,9 @@ while True:
 				
 							p3.communicate()
 				
-							print("Done processing file. Begin upload.")
+							print("Done processing file. Begin upload.", flush=True)
 						except:
-							print("Error processing BAM - ", sys.exc_info()[0])				
+							print("Error processing BAM - ", sys.exc_info()[0], flush=True)				
 
 						os.remove(tempBAMReheader)
 						os.remove(tempBAMFile)
@@ -89,7 +89,7 @@ while True:
 						message.delete()				
 				
 			else:
-				print("Message failed to provide all required attributes.")
+				print("Message failed to provide all required attributes.", flush=True)
 				print(message)
 				
 
