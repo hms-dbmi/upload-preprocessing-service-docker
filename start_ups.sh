7eea5c85a47ce045f6f39730afbc190d2c284a75
Nate Bessa <natebessa@gmail.com>
2018-08-29 15:05:42 -0400

    UDNTECH-1313: Replaces vault with AWS Secrets Manager
    Also adds a README and fixes bug where the vcf_trimmer.py file was missing in the docker container.

--- start_ups.sh
+++ start_ups.sh
@@ -1,7 +1,5 @@
 #!/usr/bin/env bash
 
-ONETIME_TOKEN=$(vault token-create -policy="ups-app-secrets" -use-limit=5 -ttl="1m" -format="json" | jq -r .auth.client_token)
-
 AWS_SECRET_ACCESS_KEY=$(aws configure get aws_secret_access_key)
 AWS_ACCESS_KEY_ID=$(aws configure get aws_access_key_id)
 
@@ -9,11 +7,8 @@ docker stop ups
 docker rm ups
 
 docker run -t -d -v $(pwd)/scratch/:/scratch --name ups \
-			-e VAULT_SKIP_VERIFY=1 \
 			-e AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY \
 			-e AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID \
-			-e ONETIME_TOKEN=$ONETIME_TOKEN \
-			-e QUEUE_NAME="upload-preprocessing-dev" \
-			-e VAULT_ADDRESS="https://vault.aws.dbmi.hms.harvard.edu:443" dbmi/upload-preprocessing-service-docker
+			-e QUEUE_NAME="upload-preprocessing" dbmi/upload-preprocessing-service-docker
 
 #docker exec -i -t ups /bin/bash
